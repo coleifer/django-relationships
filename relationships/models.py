1ec49e8372ab060c0f471fef44eea3c4c4d754d2
Charles Leifer <coleifer@gmail.com>
2010-03-22 23:44:19 -0500

    Methods added to the relationships ReverseManyRelatedManager attribute, so as not to pollute the user namespace, and since it seems more 'correct' to me.

--- relationships/models.py
+++ relationships/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
-from django.db import models
+from django.db import models, connection
+from django.db.models.fields.related import create_many_related_manager
 from relationships.constants import *
 
 class Relationship(models.Model):
@@ -11,6 +12,7 @@ class Relationship(models.Model):
 
     class Meta:
         unique_together = (('from_user', 'to_user', 'status'),)
+        ordering = ('created',)
 
     def __unicode__(self):
         return 'Relationship from %s to %s' % (self.from_user.username,
@@ -19,46 +21,69 @@ class Relationship(models.Model):
 field = models.ManyToManyField(User, through=Relationship, 
                                symmetrical=False, related_name='related_to')
 
-class UserRelationships(object):
-    def add_relationship(self, user, status=RELATIONSHIP_FOLLOWING):
+class RelationshipManager(User._default_manager.__class__):
+    def __init__(self, instance=None, *args, **kwargs):
+        self.instance = instance
+
+    def add(self, user, status=RELATIONSHIP_FOLLOWING):
         relationship, created = Relationship.objects.get_or_create(
-            from_user=self,
+            from_user=self.instance,
             to_user=user,
             status=status)
         return relationship
-    
-    def remove_relationship(self, user, status=RELATIONSHIP_FOLLOWING):
+
+    def remove(self, user, status=RELATIONSHIP_FOLLOWING):
         Relationship.objects.filter(
-            from_user=self, 
+            from_user=self.instance, 
             to_user=user,
             status=status).delete()
         return
     
     def get_relationships(self, status):
-        return self.relationships.filter(
-            to_users__status=status, 
-            to_users__from_user=self)
+        return User.objects.filter(
+            to_users__from_user=self.instance,
+            to_users__status=status)
     
     def get_related_to(self, status):
-        return self.related_to.filter(
-            from_users__status=status, 
-            from_users__to_user=self)
+        return User.objects.filter(
+            from_users__to_user=self.instance,
+            from_users__status=status)
     
-    def get_following(self):
+    def following(self):
         return self.get_relationships(RELATIONSHIP_FOLLOWING)
     
-    def get_followers(self):
+    def followers(self):
         return self.get_related_to(RELATIONSHIP_FOLLOWING)
     
-    def get_friends(self):
-        return self.relationships.filter(
+    def blocking(self):
+        return self.get_relationships(RELATIONSHIP_BLOCKING)
+    
+    def blockers(self):
+        return self.get_related_to(RELATIONSHIP_BLOCKING)
+
+    def friends(self):
+        return User.objects.filter(
             to_users__status=RELATIONSHIP_FOLLOWING, 
-            to_users__from_user=self,
+            to_users__from_user=self.instance,
             from_users__status=RELATIONSHIP_FOLLOWING, 
-            from_users__to_user=self)
+            from_users__to_user=self.instance)
+
+RelatedManager = create_many_related_manager(RelationshipManager, Relationship)
+
+class RelationshipsDescriptor(object):
+    def __get__(self, instance, instance_type=None):
+        qn = connection.ops.quote_name
+        manager = RelatedManager(
+            model=User,
+            core_filters={'related_to__pk': instance._get_pk_val()},
+            instance=instance,
+            symmetrical=False,
+            join_table=qn('relationships_relationship'),
+            source_col_name=qn('from_user_id'),
+            target_col_name=qn('to_user_id'),
+        )
+        return manager
 
 #HACK
 field.contribute_to_class(User, 'relationships')
-for name, attr in UserRelationships.__dict__.items():
-    if callable(attr):
-        setattr(User, name, attr)
+setattr(User, 'relationships', RelationshipsDescriptor())
---
+++ relationships/tests.py
@@ -0,0 +1,118 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from relationships.models import Relationship
+
+class RelationshipsTestCase(TestCase):
+    fixtures = ['relationships.json']
+    
+    def setUp(self):
+        self.walrus = User.objects.get(username='The Walrus')
+        self.john = User.objects.get(username='John')
+        self.paul = User.objects.get(username='Paul')
+        self.yoko = User.objects.get(username='Yoko')
+    
+    def test_related_names(self):
+        rel = self.walrus.relationships.all()
+        self.assertEquals(list(rel), [])
+    
+        rel = self.john.relationships.all()
+        self.assertEquals(list(rel), [self.paul, self.yoko])
+        
+        rel = self.paul.relationships.all()
+        self.assertEquals(list(rel), [self.john])
+        
+        rel = self.yoko.relationships.all()
+        self.assertEquals(list(rel), [self.john])
+        
+        rel = self.walrus.related_to.all()
+        self.assertEquals(list(rel), [])
+        
+        rel = self.john.related_to.all()
+        self.assertEquals(list(rel), [self.yoko, self.paul])
+
+        rel = self.paul.related_to.all()
+        self.assertEquals(list(rel), [self.john])
+        
+        rel = self.yoko.related_to.all()
+        self.assertEquals(list(rel), [self.john])
+    
+    def test_inherited_manager_methods(self):
+        rel = self.john.relationships.filter(username='Paul')
+        self.assertEquals(list(rel), [self.paul])
+        
+        rel = self.walrus.relationships.filter(username='Paul')
+        self.assertEquals(list(rel), [])
+        
+        rel = self.john.relationships.exclude(username='Paul')
+        self.assertEquals(list(rel), [self.yoko])
+    
+    def test_add_method(self):
+        _ = self.john.relationships.add(self.walrus)
+        
+        rel = self.john.relationships.all()
+        self.assertEquals(list(rel), [self.walrus, self.paul, self.yoko])
+        
+        rel = self.john.related_to.all()
+        self.assertEquals(list(rel), [self.yoko, self.paul])
+        
+        rel = self.walrus.relationships.all()
+        self.assertEquals(list(rel), [])
+        
+        rel = self.walrus.related_to.all()
+        self.assertEquals(list(rel), [self.john])
+        
+        # test that dupes aren't added
+        _ = self.john.relationships.add(self.walrus)
+        
+        rel = self.john.relationships.all()
+        self.assertEquals(list(rel), [self.walrus, self.paul, self.yoko])
+    
+    def test_remove_method(self):
+        self.john.relationships.remove(self.yoko)
+    
+        rel = self.john.relationships.all()
+        self.assertEquals(list(rel), [self.paul])
+        
+        rel = self.john.related_to.all()
+        self.assertEquals(list(rel), [self.yoko, self.paul])
+        
+        rel = self.yoko.relationships.all()
+        self.assertEquals(list(rel), [self.john])
+        
+        rel = self.yoko.related_to.all()
+        self.assertEquals(list(rel), [])
+        
+        # no error
+        self.john.relationships.remove(self.yoko)
+    
+    def test_custom_methods(self):
+        rel = self.john.relationships.following()
+        self.assertEquals(list(rel), [self.paul, self.yoko])
+    
+        rel = self.john.relationships.followers()
+        self.assertEquals(list(rel), [self.yoko])
+        
+        rel = self.john.relationships.blocking()
+        self.assertEquals(list(rel), [])
+    
+        rel = self.john.relationships.blockers()
+        self.assertEquals(list(rel), [self.paul])
+        
+        rel = self.john.relationships.friends()
+        self.assertEquals(list(rel), [self.yoko])
+        
+        ###
+        rel = self.paul.relationships.following()
+        self.assertEquals(list(rel), [])
+    
+        rel = self.paul.relationships.followers()
+        self.assertEquals(list(rel), [self.john])
+        
+        rel = self.paul.relationships.blocking()
+        self.assertEquals(list(rel), [self.john])
+    
+        rel = self.paul.relationships.blockers()
+        self.assertEquals(list(rel), [])
+        
+        rel = self.paul.relationships.friends()
+        self.assertEquals(list(rel), [])
