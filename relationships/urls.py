fefef43e3a6433c44cc36fd6f0a7a7791586fde4
Charles Leifer <coleifer@gmail.com>
2010-03-29 17:18:56 -0500

    Descriptive names on url patterns to allow for reverse to work

--- relationships/urls.py
+++ relationships/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('relationships.views',
     url(r'^(?P<username>[\w-]+)/(?:(?P<status_slug>[\w-]+)/)?$', 'relationship_list', name='relationship_list'),
-    url(r'^add/(?P<username>[\w-]+)/(?P<status_slug>[\w-]+)/$', 'relationship_handler', {'add': True}, name='relationship_handler'),
-    url(r'^remove/(?P<username>[\w-]+)/(?P<status_slug>[\w-]+)/$', 'relationship_handler', {'add': False}, name='relationship_handler'),
+    url(r'^add/(?P<username>[\w-]+)/(?P<status_slug>[\w-]+)/$', 'relationship_handler', {'add': True}, name='relationship_add'),
+    url(r'^remove/(?P<username>[\w-]+)/(?P<status_slug>[\w-]+)/$', 'relationship_handler', {'add': False}, name='relationship_remove'),
 )
