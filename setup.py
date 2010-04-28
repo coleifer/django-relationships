0379c51fb503ade741d5dc282074fbe107fb493f
Charles Leifer <coleifer@gmail.com>
2010-04-27 20:50:25 -0500

    Adding version info and cleaning up the setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,30 @@
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
 
+from relationships import VERSION
 
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == "":
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
 
-
-package_dir = "relationships"
-
-
-packages = []
-for dirpath, dirnames, filenames in os.walk(package_dir):
-    # ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith("."):
-            del dirnames[i]
-    if "__init__.py" in filenames:
-        packages.append(".".join(fullsplit(dirpath)))
-
-
-setup(name='django-relationships',
-    version='0.1',
-    description='Relationships between auth.users',
+setup(
+    name='django-relationships',
+    version=".".join(map(str, VERSION)),
+    description='descriptive relationships between auth.User',
+    long_description=readme,
     author='Charles Leifer',
-    #author_email='',
-    url='http://github.com/coleifer/django-relationships/',
-    packages=packages)
+    author_email='coleifer@gmail.com',
+    url='http://github.com/coleifer/django-relationships/tree/master',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
+
+
