77f2f5ba98856a440de4dad040fd35ce4b68bfc9
Arne Koesel <koesel@kono.de>
2010-05-07 11:46:28 +0200

    Fixed missing import

--- relationships/views.py
+++ relationships/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.db.models import Q
-from django.http import Http404, HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.utils import simplejson as json
