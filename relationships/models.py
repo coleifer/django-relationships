819766276a10a957fc7c26d9477028b0b685e9b5
Charles Leifer <coleifer@gmail.com>
2011-06-06 09:49:48 -0500

    Adding a weight column to the relationship model

--- relationships/models.py
+++ relationships/models.py
@@ -54,6 +54,7 @@ class Relationship(models.Model):
         related_name='to_users', verbose_name=_('to user'))
     status = models.ForeignKey(RelationshipStatus, verbose_name=_('status'))
     created = models.DateTimeField(_('created'), auto_now_add=True)
+    weight = models.FloatField(_('weight'), default=1.0, blank=True, null=True)
     site = models.ForeignKey(Site, default=settings.SITE_ID,
         verbose_name=_('site'), related_name='relationships')
 
